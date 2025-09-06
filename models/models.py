2b24c5a523c7a7a016ab5a5d3d6f1f577be8d4f3
CFuentes.Dev <151073430+cfuentes1507@users.noreply.github.com>
2025-09-06 18:48:59 -0400

    ToDo_App

--- models/models.py
+++ models/models.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
+
+class ToDo(models.Model):
+    _name = 'todo.app' #Nombre DB: todo_app
+    _description = 'Lista de Tareas'
+
+    name = fields.Char(string="Nombre")
+    status = fields.Char(string="Estado")
+    description = fields.Char(string="Descripcion")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # class to_do_app(models.Model):
